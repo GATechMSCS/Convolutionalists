aadd965168624a360c96d9dd86cb172a3e0bf5bd
Praveen Ravishankar <praveenrav@gmail.com>
2024-12-05 23:46:12 -0500

    added method to split the data for open image dataset

--- FoodforDeepThought/src/dataset_loaders/download_openimages.py
+++ FoodforDeepThought/src/dataset_loaders/download_openimages.py
@@ -1,7 +1,8 @@
 import os
 import torchvision.transforms as transforms
 from openimages.download import download_dataset
-
+import random
+import shutil
 
 class OpenImagesLoader:
     def __init__(self, random_seed = 101, batch_size = 128, perc_keep = 1.0, num_images_per_class=500):
@@ -44,3 +45,82 @@ class OpenImagesLoader:
                     print(f'An exception occured for {class_name}')
             else:
                 print(f'Skipped {class_name}, data already downloaded')
+
+    def split_data(self):
+
+        """ This function splits the downloaded Open Image dataset, and splits each class into training, validation, and testing sets.
+            This function assumes that the required data has already been downloaded."""
+
+        # Setting the random seed:
+        random.seed(self.random_seed)
+        
+        splits = ["train", "val", "test"]
+
+        # Making folders for each of the splits:
+        for split in splits:
+            split_dir = os.path.join(self.data_dir, split)
+            os.makedirs(split_dir, exist_ok=True)
+
+        # Iterating through each class:
+        for class_cur in self.classes:
+
+            # Getting directories for the images and annotations for each class:
+            imgs_dir = os.path.join(self.data_dir, class_cur, "images")
+            anns_dir = os.path.join(self.data_dir, class_cur, "pascal")
+
+            # Ensuring each class has images and annotations:
+            if not imgs_dir:
+                print(f"Images do not exist for {class_cur}!")
+                continue
+
+            if not anns_dir:
+                print(f"Annotations do not exist for {class_cur}!")
+                continue
+
+            class_imgs = os.listdir(imgs_dir) # Images for current class
+            class_anns = os.listdir(anns_dir) # Annotations for current class
+
+            num_imgs = len(class_imgs) # Number of images and annotations for current class
+            
+            # Shuffling data:
+
+            inds_list = list(range(num_imgs)) # List of indices ranging for the total number of images
+            random.shuffle(inds_list) # Shuffling indices list
+            class_imgs = class_imgs[inds_list] # Shuffling class images according to shuffled inds_list
+            class_anns = class_anns[inds_list] # Shuffling class annotations according to shuffled inds_list
+                        
+            ind_train = int(0.8 * num_imgs) # Ending index for the training images
+            ind_val = ind_train + int(0.1 * num_imgs) # Ending index for the validation images
+
+            # Splitting images into training, validation, and testing:
+            train_imgs = class_imgs[:ind_train]
+            val_imgs = class_imgs[ind_train:ind_val]
+            test_imgs = class_imgs[ind_val:]
+
+            all_imgs = [(train_imgs, val_imgs, test_imgs] # All images
+            
+            # Splitting annotations into training, validation, and testing:
+            train_anns = class_anns[:ind_train]
+            val_anns = class_anns[ind_train:ind_val]
+            test_anns = class_anns[ind_val:]
+
+            all_anns = [train_anns, val_anns, test_anns] # All annotations
+            
+            # Looping through all split types and corresponding split images:
+            for split_type, split_imgs, split_anns in zip(splits, all_imgs, all_anns):
+
+                # Creating each split directory for images and annotations for current class:
+                split_dir_img = os.path.join(self.data_dir, split_type, class_cur, "images")
+                split_dir_ann = os.path.join(self.data_dir, split_type, class_cur, "annotations")
+
+                os.makedirs(split_dir_img, exist_ok=True)
+                os.makedirs(split_dir_ann, exist_ok=True)
+
+                # Copying each image from initial directory to corresponding split directory for each split:
+                for img, ann in zip(split_imgs, split_anns):
+                    shutil.copy(os.path.join(imgs_dir, img), os.path.join(split_dir_img, img))
+                    shutil.copy(os.path.join(anns_dir, ann), os.path.join(split_dir_ann, ann))
+            
+
+
+        
