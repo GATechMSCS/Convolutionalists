8e1888a69b7f33f4f510719b2f9c4824c611fd3f
scottschmidl <sjschmidl@gmail.com>
2024-12-05 21:43:54 -0600

    changed file name

--- FoodforDeepThought/src/dataset_loaders/download_data.py
+++ FoodforDeepThought/src/dataset_loaders/download_data.py
@@ -1,53 +1,35 @@
-from openimages.download import download_images
+# from openimages.download import download_images, download_dataset
+from download_openimages import OpenImagesLoader
+import pandas as pd
 
-food_categories = [
-    # Fruits:
-    "Apple", "Banana", "Orange", "Pear", "Strawberry", "Blueberry", 
-    "Raspberry", "Blackberry", "Lemon", "Lime", "Pineapple", "Mango", 
-    "Watermelon", "Grapes", "Peach", "Cherry", "Papaya", "Kiwi", 
-    "Plum", "Avocado", "Coconut", "Pomegranate",
+# path_to_csv = "FoodforDeepThought/data/class-descriptions-boxable.csv"
+# food_categories = pd.read_csv(path_to_csv, header=None)
+# print(food_categories[0].tolist())
 
-    # Vegetables:
-    "Broccoli", "Carrot", "Potato", "Onion", "Tomato", "Cucumber", 
-    "Bell pepper", "Lettuce", "Kale", "Spinach", "Zucchini", "Eggplant", 
-    "Cauliflower", "Corn", "Sweet potato", "Celery", "Asparagus", 
-    "Green beans", "Garlic", "Radish", "Beet", "Mushroom", 
-    "Chili pepper",
+# food_categories =  ["French fries", "Waffle", "Pancake", "Burrito", "Snack", "Pretzel", 
+#                     "Popcorn", "Cookie", "Dessert", "Muffin", "Ice cream", "Cake", "Candy", 
+#                     "Guacamole", "Fruit", "Apple", "Grape", "Common fig", "Pear", 
+#                     "Strawberry", "Tomato", "Lemon", "Banana", "Orange", "Peach", "Mango", 
+#                     "Pineapple", "Grapefruit", "Pomegranate", "Watermelon", "Cantaloupe", 
+#                     "Egg", "Baked goods", "Bagel", "Bread", "Pastry", "Doughnut", "Croissant", 
+#                     "Tart", "Mushroom", "Pasta", "Pizza", "Seafood", "Squid", "Shellfish", 
+#                     "Oyster", "Lobster", "Shrimp", "Crab", "Taco", "Cooking spray", 
+#                     "Vegetable", "Cucumber", "Radish", "Artichoke", "Potato", "Asparagus", 
+#                     "Squash", "Pumpkin", "Zucchini", "Cabbage", "Carrot", "Salad", 
+#                     "Broccoli", "Bell pepper", "Winter melon", "Honeycomb", "Sandwich", 
+#                     "Hamburger", "Submarine sandwich", "Dairy", "Cheese", "Milk", "Sushi"]
 
-    # Nuts and Seeds:
-    "Almond", "Cashew", "Peanut", "Walnut", "Hazelnut", "Pecan", 
-    "Pistachio", "Sunflower seed", "Pumpkin seed", "Sesame seed", 
-    "Chia seed",
+# pathtoimgs = "FoodforDeepThought/data"
+# download_dataset(dest_dir=pathtoimgs,
+#                  class_labels=food_categories,
+#                  annotation_format='pascal',
+#                  limit=1000)
 
-    # Meals:
-    "Pizza", "Hamburger", "Hot dog", "Sandwich", "Taco", "Burrito", 
-    "Cake", "Cookie", "Donut", "Ice cream", "Pancake", "Waffle", 
-    "Muffin", "Cupcake", "French fries", "Sushi", "Ramen", "Fried rice", 
-    "Curry", "Soup", "Salad", "Pasta", "Spaghetti", "Macaroni and cheese", 
-    "Lasagna", "Bread", "Bagel", "Croissant",
+def main():
+    oil = OpenImagesLoader(random_seed=101,
+                           batch_size=128,
+                           perc_keep=1.0)
+    oil.download_data    
 
-    # Beverages:
-    "Coffee", "Tea", "Milk", "Juice", "Smoothie", "Beer", "Wine", 
-    "Cocktail", "Water bottle", "Soda can",
-
-    # Snacks and Desserts:
-    "Chocolate", "Candy", "Popcorn", "Chips", "Pretzel", "Granola bar", 
-    "Ice cream cone", "Cheesecake", "Brownie", "Pie",
-
-    # Dairy and Meat:
-    "Cheese", "Butter", "Yogurt", "Egg", "Bacon", "Chicken", "Beef", 
-    "Pork", "Fish", "Shrimp", "Crab", "Lobster", "Steak", "Sausage", 
-    "Turkey", "Lamb",
-
-    # Seafood:
-    "Salmon", "Tuna", "Prawns", "Oysters", "Mussels", "Clams", "Scallops",
-
-    # Bread and Cereal:
-    "Rice", "Bread loaf", "Tortilla", "Noodles", "Flour", "Sugar", 
-    "Oats", "Cereal", "Cornbread",
-    "Ketchup", "Mustard", "Mayonnaise", "Soy sauce", "Hot sauce", 
-    "Honey", "Peanut butter", "Jam", "Salt", "Pepper", "Cinnamon", 
-    "Curry powder", "Garlic powder", "Vanilla extract"]
-
-pathtoimgs = "FoodforDeepThought/data"
-download_images(pathtoimgs, food_categories)
+if __name__ == "__main__":
+    main()
