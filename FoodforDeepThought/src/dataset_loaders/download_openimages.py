7c0c9cf1e948571f349d35230bff3c8b9b6cd249
Praveen Ravishankar <praveenrav@gmail.com>
2024-12-05 22:50:49 -0500

    minor changes to open images downloader

--- FoodforDeepThought/src/dataset_loaders/download_openimages.py
+++ FoodforDeepThought/src/dataset_loaders/download_openimages.py
@@ -4,17 +4,18 @@ from openimages.download import download_dataset
 
 
 class OpenImagesLoader:
-    def __init__(self, random_seed=101, batch_size=128, perc_keep=1.0):
+    def __init__(self, random_seed = 101, batch_size = 128, perc_keep = 1.0, num_images_per_class=500):
         self.data_dir = os.path.join("data", "openimages")  # Directory in which dataset resides
         self.csv_dir = os.path.join("data", "openimages_csv_dir")
         self.random_seed = random_seed
         self.batch_size = batch_size
         self.perc_keep = perc_keep  # Percentage of dataset to be kept (number between 0 and 1)
+        self.num_images_per_class = 750
         self.transforms = transforms.Compose(
             [
                 transforms.Resize((224, 224)),
-                transforms.ToTensor()
-                # transforms.Normalize
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]) # ImageNet's normalization statistics
             ]
         )
 
@@ -38,7 +39,7 @@ class OpenImagesLoader:
             print(f'Attempting to download {class_name} data')
             if not os.path.isdir(os.path.join(self.data_dir, class_name)):
                 try:
-                    download_dataset(self.data_dir, [class_name], None, annotation_format="darknet", csv_dir=None, limit=750)
+                    download_dataset(self.data_dir, [class_name], None, annotation_format="darknet", csv_dir=None, limit=500)
                 except:
                     print(f'An exception occured for {class_name}')
             else:
